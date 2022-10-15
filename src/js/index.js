faf724c8eb6718f45284d430092bc11d80a90ad6
Jorge H <jorgehenrrique@live.com>
2022-10-15 02:52:46 -0300

    finalizando js

--- src/js/index.js
+++ src/js/index.js
@@ -12,14 +12,36 @@
 const abas = document.querySelectorAll(".aba"); //passo 1
 
 abas.forEach(aba => { //passo 2
-    aba.addEventListener("click", function() { //passo3
+    aba.addEventListener("click", function() {
 
         if(aba.classList.contains("selecionado")){
             return;
         }
-        const abaSelecionada = document.querySelector(".aba.selecionado");
-        abaSelecionada.classList.remove("selecionado");
+        
+        selecionarAba(aba)
 
-        aba.classList.add("selecionado"); //passo 4
+        mostrarInformacoesDaAba(aba)
     });
 });
+
+function selecionarAba(aba) {
+
+    //passo 3
+    const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado");
+    
+    //passo 4
+    aba.classList.add("selecionado");
+}
+
+function mostrarInformacoesDaAba(aba){
+    //passo 5
+    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+    informacaoSelecionada.classList.remove("selecionado");
+    
+    //passo 6
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+    
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
+    informacaoASerMostrada.classList.add("selecionado");
+}
