99c482e4dab42f422043a2e673bf2d96deb4a451
Jorge H <jorgehenrrique@live.com>
2023-02-27 15:57:13 -0300

    reajustado nome de variavel

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,25 @@
-const sobre = document.querySelector('#sobre-mim');
-const redes = document.querySelector('#minhas-redes');
+const abaSobre = document.querySelector('#sobre-mim');
+const abaRedes = document.querySelector('#minhas-redes');
 
 const infoSobre = document.querySelector('#informacao-sobre-mim');
 const infoRedes = document.querySelector('#informacao-minhas-redes');
 
-sobre.addEventListener('click', function () {
+abaSobre.addEventListener('click', function () {
     if (infoSobre.classList.contains('selecionado')) return;
 
     infoSobre.classList.add('selecionado');
     infoRedes.classList.remove('selecionado');
 
-    sobre.classList.add('selecionado');
-    redes.classList.remove('selecionado');
+    abaSobre.classList.add('selecionado');
+    abaRedes.classList.remove('selecionado');
 });
 
-redes.addEventListener('click', function () {
+abaRedes.addEventListener('click', function () {
     if (infoRedes.classList.contains('selecionado')) return;
 
     infoRedes.classList.add('selecionado');
     infoSobre.classList.remove('selecionado');
 
-    redes.classList.add('selecionado');
-    sobre.classList.remove('selecionado');
+    abaRedes.classList.add('selecionado');
+    abaSobre.classList.remove('selecionado');
 });
