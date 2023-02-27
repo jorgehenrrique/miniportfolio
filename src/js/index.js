7332fc9dc95ea748d7fe6109b81701382f24006f
Jorge H <jorgehenrrique@live.com>
2023-02-27 15:45:47 -0300

    rmv comentarios

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 /*
     OBJETIVO - quando clicarmos na aba temos que mostrar o conteúdo da aba que foi clicada pelo usuário e esconder o conteúdo da aba anterior
 
-    - passo 1 - dar um jeito de pegar os elementos que representam as abas no HTML
-    - passo 2 - dar um jeito de identificar o clique no elemento da aba
+    - passo 1 - pegar os elementos que representam as abas no HTML
+    - passo 2 - identificar o clique no elemento da aba
     - passo 3 - quando o usuário clicar, desmarcar a aba selecionada
     - passo 4 - marcar a aba clicada como selecionado
     - passo 5 - esconder o conteúdo anterior
